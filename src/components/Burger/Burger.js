032893d077fc87895fb3bc83a4aa09fcd44d427f
Deniss Samoilov <denis@hebsdigital.com>
2019-01-21 08:00:31 +0200

    Lecture 122

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from './Burger.css';
 import BurgerIngerdient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map( (_, i) => {
             return <BurgerIngerdient key={igKey + i} type={igKey} />;
         } )
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
